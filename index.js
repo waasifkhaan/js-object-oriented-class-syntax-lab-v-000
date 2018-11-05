c4a9515fdf4815ea2f1641902fb0e7790cf5fcb5
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:29:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,23 +9,23 @@ class BoardMember {
     return `No, I must disagree`;
   }
   approve() {
-    console.log(`You can do that!`);
+    return `You can do that!`;
   }
   charity() {
-    console.log(`I like to help people.`);
+    return `I like to help people.`;
   }
   releasePressStatement() {
-    console.log(`You will see great things from Scuber.`);
+    return `You will see great things from Scuber.`;
   }
   sayHi() {
-    console.log(`Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in #{this.training}.`);
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in #{this.training}.`;
   }
 }
 
   class Ceo extends BoardMember {
 
     hireEmployee() {
-      console.log('Welcome aboard!')
+      return 'Welcome aboard!';
     }
 
   }
