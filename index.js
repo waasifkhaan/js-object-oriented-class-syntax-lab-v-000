c85243bcf49c2001fd2a1297afb1109eb4c26a9f
Wasif Khan <waasif.khaan@gmail.com>
2018-11-05 16:22:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,26 @@
+class BoardMember {
+  constructor(name, homeState, training) {
+    this.name = name;
+    this.homeState = homeState;
+    this.training = training
+  }
+ 
+  veto() {
+    console.log(`No, I must disagree`);
+  }
+  approve() {
+    console.log(`You can do that!`);
+  }
+  charity() {
+    console.log(`I like to help people.`);
+  }
+  releasePressStatement() {
+    console.log(``);
+  }
+  sayHi() {
+    console.log(``);
+  }
+
+
+
+}
